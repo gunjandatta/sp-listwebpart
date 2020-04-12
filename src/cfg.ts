5cb8820b6e3acce4636103e71bf6f793a1f0f4f0
Gunjan Datta <me@dattabase.com>
2020-04-12 11:12:06 -0400

    Updated the configuration to use the new generate XML methods for the script editor webpart.

--- src/cfg.ts
+++ src/cfg.ts
@@ -61,29 +61,17 @@ export const Configuration = {
             {
                 FileName: "demo-list.webpart",
                 Group: "Demo",
-                XML:
-                    `<?xml version="1.0" encoding="utf-8"?>
-<webParts>
-    <webPart xmlns="http://schemas.microsoft.com/WebPart/v3">
-        <metaData>
-            <type name="Microsoft.SharePoint.WebPartPages.ScriptEditorWebPart, Microsoft.SharePoint, Version=15.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" />
-            <importErrorMessage>$Resources:core,ImportantErrorMessage;</importErrorMessage>
-        </metaData>
-        <data>
-            <properties>
-                <property name="Title" type="string">Demo List</property>
-                <property name="Description" type="string">Displays the demo list data.</property>
-                <property name="ChromeType" type="chrometype">None</property>
-                <property name="Content" type="string">
-                    &lt;div id="wp-demolist"&gt;&lt;/div&gt;
-                    &lt;div id="wp-demolistcfg" style="display: none;"&gt;&lt;/div&gt;
-                    &lt;script type="text/javascript" src="/sites/dev/siteassets/sp-examples/demoListWP.js"&gt;&lt;/script&gt;
-                    &lt;script type="text/javascript"&gt;SP.SOD.executeOrDelayUntilScriptLoaded(function() { new WPList(); }, 'demoListWP');&lt;/script&gt;
-                </property>
-            </properties>
-        </data>
-    </webPart>
-</webParts>`
+                XML: Helper.WebPart.generateScriptEditorXML({
+                    chromeType: "None",
+                    description: "Displays the demo list data.",
+                    title: "Demo List",
+                    content: [
+                        '&lt;div id="wp-demolist"&gt;&lt;/div&gt;',
+                        '&lt;div id="wp-demolistcfg" style="display: none;"&gt;&lt;/div&gt;',
+                        '&lt;script type="text/javascript" src="/sites/dev/siteassets/sp-examples/demoListWP.js"&gt;&lt;/script&gt;',
+                        '&lt;script type="text/javascript"&gt;SP.SOD.executeOrDelayUntilScriptLoaded(function() { new WPList(); }, \'demoListWP\');&lt;/script&gt;'
+                    ].join('\n')
+                })
             }
         ]
     })
